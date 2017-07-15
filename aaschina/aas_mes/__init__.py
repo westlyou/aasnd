891b072d82a1689ae6e9d415060322cd444aca40
luforn <lufornlee@gmail.com>
2017-07-15 21:56:00 +0800

    Add Mes

---
+++ aaschina/aas_mes/__init__.py
@@ -0,0 +1,10 @@
+# -*-  coding: utf-8 -*-
+
+"""
+@version: 1.0
+@author: luforn
+@license: LGPL V3
+@time: 2017-7-15 17:54
+"""
+
+import models
---
+++ aaschina/aas_mes/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': u'安费诺生产管理',
+    'version': '1.0',
+    'category': 'Zhiq Amphenol',
+    'sequence': 5,
+    'summary': u'安费诺生产管理',
+    'description': """
+        安费诺生产管理
+    """,
+    'author': 'Luforn',
+    'website': 'http://www.zhiq.info',
+    'depends': ['aas_wms'],
+    'data': [
+        'security/aas_mes_security.xml',
+        'security/ir.model.access.csv',
+        'views/aas_mes_view.xml',
+        'views/aas_stock_delivery_view.xml'
+    ],
+    'qweb':[],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'external_dependencies': {'python': ['wechatpy']}
+}
