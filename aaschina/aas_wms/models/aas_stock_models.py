eb3f0d6401c7bb139a7cefc520e77e8d9b94ae6a
luforn <lufornlee@gmail.com>
2017-07-19 20:43:00 +0800

    Update Receipt Label Print

--- aaschina/aas_wms/models/aas_stock_models.py
+++ aaschina/aas_wms/models/aas_stock_models.py
@@ -149,6 +149,3 @@ class AASStockQuantReport(models.Model):
         drop_view_if_exists(self._cr, self._table)
         self._cr.execute("""CREATE or REPLACE VIEW %s as ( %s FROM %s %s )""" % (self._table, self._select(), self._from(), self._group_by()))
 
-
-
-
