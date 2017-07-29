b6adc8e2526087b7bf18ed3e8aca6d354c35b7dc
luforn <lufornlee@gmail.com>
2017-07-29 18:31:15 +0800

    Add Stock Inventory

--- aaschina/aas_wms/wechat/controllers/aas_inventory.py
+++ aaschina/aas_wms/wechat/controllers/aas_inventory.py
@@ -66,8 +66,6 @@ class AASInventoryWechatController(http.Controller):
     @http.route('/aaswechat/wms/inventorylabelscan', type='json', auth="user")
     def aas_wechat_wms_inventorylabelscan(self, inventoryid, barcode):
         values = {'success': True, 'message': ''}
-        _logger.info('barcode: '+barcode)
-        _logger.info('inventoryid: '+str(inventoryid))
         inventory = request.env['aas.stock.inventory'].browse(inventoryid)
         if not inventory:
             values.update({'success': False, 'message': u'请仔细检查盘点单可能被删除了！'})
