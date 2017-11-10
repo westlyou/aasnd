570053e2d75dbc257844d017ab5020e66108d16b
luforn <lufornlee@gmail.com>
2017-11-10 10:42:36 +0800

    add wechat jssdk init

--- aaschina/aas_mes/static/wechat/wechat_mes_workticketfinish.js
+++ aaschina/aas_mes/static/wechat/wechat_mes_workticketfinish.js
@@ -27,6 +27,20 @@ mui.ready(function(){
         document.getElementById('action_scancontainer').style.display = 'block';
     }
 
+    mui.ajax('/aaswechat/mes/scaninit',{
+        data: JSON.stringify({
+            jsonrpc: "2.0", method: 'call', params: {'access_url': location.href}, id: Math.floor(Math.random() * 1000 * 1000 * 1000)
+        }),
+        dataType:'json', type:'post', timeout:10000,
+        headers:{'Content-Type':'application/json'},
+        success:function(data){
+            wx.config(data.result);
+            wx.ready(function(){});
+            wx.error(function(res){});
+        },
+        error:function(xhr,type,errorThrown){ console.log(type); }
+    });
+
 
     //加载动画
     function aas_finish_loading(){
