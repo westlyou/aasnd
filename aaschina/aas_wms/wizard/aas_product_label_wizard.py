d6d811f428dfcd8795a3d868d24de183ab33c85e
luforn <lufornlee@gmail.com>
2017-12-11 21:17:48 +0800

    update label split

--- aaschina/aas_wms/wizard/aas_product_label_wizard.py
+++ aaschina/aas_wms/wizard/aas_product_label_wizard.py
@@ -27,7 +27,7 @@ class AASProductLabelSplitWizard(models.TransientModel):
     product_lot = fields.Many2one(comodel_name='stock.production.lot', string=u'批次', ondelete='cascade')
     product_qty = fields.Float(string=u'数量', digits=dp.get_precision('Product Unit of Measure'))
     label_qty = fields.Float(string=u'每标签数量', digits=dp.get_precision('Product Unit of Measure'))
-    label_count = fields.Integer(string=u'标签个数')
+    label_count = fields.Integer(string=u'标签个数', default=1)
 
     @api.multi
     def action_done(self):
