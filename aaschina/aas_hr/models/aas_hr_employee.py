4d274922f3848b228c0ab260495823987a26cf9b
luforn <lufornlee@gmail.com>
2017-12-16 14:06:01 +0800

    update employee

--- aaschina/aas_hr/models/aas_hr_employee.py
+++ aaschina/aas_hr/models/aas_hr_employee.py
@@ -111,6 +111,6 @@ class AASHREmployee(models.Model):
 
     @api.multi
     def action_dimission(self):
-        self.write({'dimission_time': fields.Datetime.now(), 'state': 'dimission'})
+        self.write({'dimission_time': fields.Datetime.now(), 'state': 'dimission', 'active': False})
 
 
