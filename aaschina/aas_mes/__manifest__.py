9bcd2d0c659b0bd34e926e26d2ae0f63b88bf4e9
luforn <lufornlee@gmail.com>
2018-01-10 12:22:39 +0800

    add mes sundryin

--- aaschina/aas_mes/__manifest__.py
+++ aaschina/aas_mes/__manifest__.py
@@ -52,7 +52,7 @@
         'views/aas_mes_attendance_view.xml',
         'views/aas_mes_settings_view.xml',
         'views/aas_mes_container_view.xml',
-        'views/aas_mes_production_label_view.xml',
+        'views/aas_mes_production_view.xml',
 
         'oracle/aas_ebs_mainorder_view.xml',
         'wechat/views/aas_wechat_mes_template.xml',
