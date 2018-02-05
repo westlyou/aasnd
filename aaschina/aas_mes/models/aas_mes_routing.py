d1961f40420acae76822bd6ec959acb509b63012
luforn <lufornlee@gmail.com>
2018-02-05 17:25:20 +0800

    add routing build workticket

--- aaschina/aas_mes/models/aas_mes_routing.py
+++ aaschina/aas_mes/models/aas_mes_routing.py
@@ -30,6 +30,7 @@ class AASMESRouting(models.Model):
     active = fields.Boolean(string=u'有效', default=True, copy=False)
     version = fields.Char(string=u'版本', copy=False)
     note = fields.Text(string=u'描述')
+    workticket = fields.Boolean(string=u'是否生成工票', default=True, copy=False)
     state = fields.Selection(selection=ROUTSTATE, string=u'状态', default='draft', copy=False)
     create_time = fields.Datetime(string=u'创建时间', default=fields.Datetime.now, copy=False)
     mesline_id = fields.Many2one(comodel_name='aas.mes.line', string=u'产线', ondelete='restrict')
@@ -39,16 +40,9 @@ class AASMESRouting(models.Model):
     routing_lines = fields.One2many(comodel_name='aas.mes.routing.line', inverse_name='routing_id', string=u'工艺工序')
 
     @api.model
-    def action_checking_version(self):
-        tz_name = self.env.user.tz or self.env.context.get('tz') or 'Asia/Shanghai'
-        utctime = fields.Datetime.from_string(fields.Datetime.now())
-        utctime = pytz.timezone('UTC').localize(utctime, is_dst=False)
-        currenttime = utctime.astimezone(pytz.timezone(tz_name))
-        return currenttime.strftime('%Y%m%d')
-
-    @api.model
     def create(self, vals):
-        vals['version'] = self.action_checking_version()
+        chinadate = fields.Datetime.to_china_today()
+        vals['version'] = chinadate.replace('-', '')
         return super(AASMESRouting, self).create(vals)
 
     @api.one
