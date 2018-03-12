1a72bf99b310e972f016793b26a12de18b8b3c75
luforn <lufornlee@gmail.com>
2018-03-12 21:00:53 +0800

    add workorder report

--- aaschina/aas_mes/models/aas_mes_report.py
+++ aaschina/aas_mes/models/aas_mes_report.py
@@ -18,39 +18,39 @@ from odoo.tools.sql import drop_view_if_exists
 
 _logger = logging.getLogger(__name__)
 
-# class AASMESProducePlanReport(models.Model):
-#     _auto = False
-#     _name = 'aas.mes.produce.plan.report'
-#     _description = 'AAS MES Produce Plan Report'
-#
-#
-#     mesline_id = fields.Many2one(comodel_name='aas.mes.line', string=u'产线', index=True)
-#     mesline_name = fields.Char(string=u'产线名称', copy=False)
-#     product_id = fields.Many2one(comodel_name='product.product', string=u'产品', index=True)
-#     product_code = fields.Char(string=u'产品编码', copy=False)
-#     produce_date = fields.Char(string=u'生产日期', copy=False)
-#     plan_qty = fields.Float(string=u'计划数量', digits=dp.get_precision('Product Unit of Measure'), default=0.0)
-#
-#
-#     def _select_sql(self):
-#         _select_sql = """
-#         SELECT MIN(workorder.id) AS id,
-#         workorder.mesline_id AS mesline_id,
-#         workorder.mesline_name AS mesline_name,
-#         workorder.product_id AS product_id,
-#         workorder.product_code AS product_code,
-#         workorder.produce_date AS produce_date,
-#         sum(workorder.input_qty) AS plan_qty
-#         FROM aas_mes_workorder workorder
-#         GROUP BY workorder.mesline_id, workorder.mesline_name, workorder.product_id, workorder.product_code, workorder.produce_date
-#         """
-#         return _select_sql
-#
-#
-#     @api.model_cr
-#     def init(self):
-#         drop_view_if_exists(self._cr, self._table)
-#         self._cr.execute("""CREATE or REPLACE VIEW %s as ( %s )""" % (self._table, self._select_sql()))
+class AASMESProducePlanReport(models.Model):
+    _auto = False
+    _name = 'aas.mes.produce.plan.report'
+    _description = 'AAS MES Produce Plan Report'
+
+
+    mesline_id = fields.Many2one(comodel_name='aas.mes.line', string=u'产线', index=True)
+    mesline_name = fields.Char(string=u'产线名称', copy=False)
+    product_id = fields.Many2one(comodel_name='product.product', string=u'产品', index=True)
+    product_code = fields.Char(string=u'产品编码', copy=False)
+    produce_date = fields.Char(string=u'生产日期', copy=False)
+    plan_qty = fields.Float(string=u'计划数量', digits=dp.get_precision('Product Unit of Measure'), default=0.0)
+
+
+    def _select_sql(self):
+        _select_sql = """
+        SELECT MIN(workorder.id) AS id,
+        workorder.mesline_id AS mesline_id,
+        workorder.mesline_name AS mesline_name,
+        workorder.product_id AS product_id,
+        workorder.product_code AS product_code,
+        workorder.produce_date AS produce_date,
+        sum(workorder.input_qty) AS plan_qty
+        FROM aas_mes_workorder workorder
+        GROUP BY workorder.mesline_id, workorder.mesline_name, workorder.product_id, workorder.product_code, workorder.produce_date
+        """
+        return _select_sql
+
+
+    @api.model_cr
+    def init(self):
+        drop_view_if_exists(self._cr, self._table)
+        self._cr.execute("""CREATE or REPLACE VIEW %s as ( %s )""" % (self._table, self._select_sql()))
 
 
 
