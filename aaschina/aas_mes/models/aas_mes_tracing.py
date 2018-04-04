51026153da65f6c38a309ec171e80783a36e8c13
luforn <lufornlee@gmail.com>
2018-04-04 10:11:45 +0800

    update tracing

--- aaschina/aas_mes/models/aas_mes_tracing.py
+++ aaschina/aas_mes/models/aas_mes_tracing.py
@@ -22,6 +22,7 @@ _logger = logging.getLogger(__name__)
 # 产出消耗报表
 class AASProductionProductMaterialReport(models.Model):
     _auto = False
+    _rec_name = 'product_id'
     _name = 'aas.production.product.material.report'
     _description = 'AAS Production Product Material Report'
     _order = 'mesline_id,product_id,product_lot,material_id,material_lot'
@@ -61,7 +62,7 @@ class AASProductionProductMaterialReport(models.Model):
         mdomain = [('product_id', '=', self.material_id.id), ('product_lot', '=', self.material_lot.id)]
         if self.env['aas.production.product.material.report'].search_count(mdomain) <= 0:
             raise UserError(u'当前没有原料信息可以追溯！')
-        tempdomain = "[('product_id','=',"+str(self.material_id.id)+"),('product_lot','=',"+str(self.material_lot.id)+")]"
+        tempdomain = [('product_id', '=', self.material_id.id), ('product_lot', '=', self.material_lot.id)]
         view_form = self.env.ref('aas_mes.view_form_aas_production_product_material_report')
         view_tree = self.env.ref('aas_mes.view_tree_aas_production_product_material_report')
         return {
@@ -85,7 +86,7 @@ class AASProductionProductMaterialReport(models.Model):
         pdomain = [('material_id', '=', self.product_id.id), ('material_lot', '=', self.product_lot.id)]
         if self.env['aas.production.product.material.report'].search_count(pdomain) <= 0:
             raise UserError(u'当前没有成品信息可以追溯！')
-        tempdomain = "[('material_id','=',"+str(self.product_id.id)+"),('material_lot','=',"+str(self.product_lot.id)+")]"
+        tempdomain = [('material_id', '=', self.product_id.id), ('material_lot', '=', self.product_lot.id)]
         view_form = self.env.ref('aas_mes.view_form_aas_production_product_material_report')
         view_tree = self.env.ref('aas_mes.view_tree_aas_production_product_material_report')
         return {
@@ -178,19 +179,6 @@ class AASProductionForwardTracingWizard(models.TransientModel):
         self.ensure_one()
         if not self.serialnumber_id and (not self.product_id or not self.product_lot):
             raise UserError(u'如果未设置序列号，请设置好成品和批次信息！')
-        # tempdomain = "["
-        # if self.serialnumber_id:
-        #     mesline = self.serialnumber_id.mesline_id
-        #     product, plot = self.serialnumber_id.product_id, self.serialnumber_id.product_lot
-        #     tempdomain += "('mesline_id','=',"+str(mesline.id)+"),('product_id','=',"+str(product.id)+")"
-        #     if plot:
-        #         tempdomain += ",('product_lot','=',"+str(plot.id)+")"
-        #     else:
-        #         tempdomain += ",('product_lot','=',False)"
-        # else:
-        #     tempdomain += "('product_id','=',"+str(self.product_id.id)+")"
-        #     tempdomain += ",('product_lot','=',"+str(self.product_lot.id)+")"
-        # tempdomain += "]"
         tempdomain = []
         serialnumber, product, prolot = self.serialnumber_id, self.product_id, self.product_lot
         if serialnumber:
@@ -199,25 +187,25 @@ class AASProductionForwardTracingWizard(models.TransientModel):
             tempdomain += [('mesline_id', '=', mesline.id), ('product_id', '=', tproduct.id)]
             if tprolot:
                 tempdomain += [('product_lot', '=', tprolot.id)]
-            # else:
-            #     tempdomain += [('product_lot', '=', False)]
+            else:
+                tempdomain += [('product_lot', '=', False)]
         else:
             tempdomain += [('product_id', '=', product.id), ('product_lot', '=', prolot.id)]
-        view_form = self.env.ref('aas_mes.view_tree_aas_production_product_material_report')
-        view_tree = self.env.ref('aas_mes.view_form_aas_production_product_material_report')
-        print 'im here ...............'
-        return {
-            'name': u"正向追溯",
-            'type': 'ir.actions.act_window',
-            'view_mode': 'tree,form',
-            'res_model': 'aas.production.product.material.report',
-            'views': [(view_tree.id, 'tree'), (view_form.id, 'form')],
-            'target': 'self',
-            'context': self.env.context,
-            'domain': tempdomain
-        }
+        action = self.env.ref('aas_mes.action_aas_production_product_material_report').read()[0]
+        action['domain'] = tempdomain
+        return action
 
 
+class AASProductionReverseTracingWizard(models.TransientModel):
+    _name = 'aas.production.reverse.tracing.wizard'
+    _description = 'AAS Production Reverse Tracing Wizard'
+
+    material_id = fields.Many2one(comodel_name='product.product', string=u'原料')
+    material_lot = fields.Many2one(comodel_name='stock.production.lot', string=u'批次')
+
+    @api.multi
+    def action_tracing(self):
+        self.ensure_one()
 
 
 
