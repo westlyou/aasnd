60fbd8fc528d54d735505a9a94f323218f7349d8
luforn <lufornlee@gmail.com>
2018-04-29 19:04:49 +0800

    update workstation equipment

--- aaschina/aas_mes/models/aas_mes_production.py
+++ aaschina/aas_mes/models/aas_mes_production.py
@@ -321,10 +321,12 @@ class AASProductionProduct(models.Model):
         if not mesline or not workstation:
             return values
         employeedomain = [('workstation_id', '=', workstation.id)]
-        if equipment:
-            employeedomain.append(('equipment_id', '=', equipment.id))
         if not workstation.ispublic:
             employeedomain.append(('mesline_id', '=', mesline.id))
+        if equipment:
+            tempdomain = employeedomain + [('equipment_id', '=', equipment.id)]
+            if self.env['aas.mes.workstation.employee'].search_count(employeedomain) > 0:
+                employeedomain = tempdomain
         employeelist = self.env['aas.mes.workstation.employee'].search(employeedomain)
         if employeelist and len(employeelist) > 0:
             employeeids, employeelines = [], []
